9fee1ea00fb18dcd69294484098bc48e92b4893b
corinnedixon <67010820+corinnedixon@users.noreply.github.com>
2020-06-18 15:20:31 -0400

    Test num for weight

--- cheesescalev2.py
+++ cheesescalev2.py
@@ -141,7 +141,7 @@ def readWeight():
                 b3 = b2[b2.find(":") + 2:b2.find(":") + 9].strip()
                 
                 try:
-                    x = round(float(b3) * fac * 2.20462,2)
+                    x = round(float(b3) * fac * 5.20462,2)
                     scaleWeight.set(x)
                 except ValueError:
                     pass
