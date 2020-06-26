fda6ba23fa288a199d6493553e817901e535b920
corinnedixon <67010820+corinnedixon@users.noreply.github.com>
2020-06-26 13:09:18 -0400

    Set up proper pin

--- cheesescaletest1.py
+++ cheesescaletest1.py
@@ -47,7 +47,7 @@ GPIO.setup(button14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 #Set up 3 position switch at IO pins and on/off switch
 GPIO.setup(13,GPIO.IN)
 GPIO.setup(15,GPIO.IN)
-GPIO.setup(37,GPIO.IN)
+GPIO.setup(22,GPIO.IN)
 
 #Set up these pins to output high to buttons and on/off switch
 GPIO.setwarnings(False)
