b249c572dfe70cdf6173439cb024a13d3c2b8d1c
corinnedixon <67010820+corinnedixon@users.noreply.github.com>
2020-07-01 13:47:41 -0400

    back to 21

--- cheesescaletest1.py
+++ cheesescaletest1.py
@@ -30,7 +30,6 @@ firebase = pyfireconnect.initialize(config)
 db = firebase.database()
 
 #Board set up
-GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
 
 #Input pins for each button
@@ -47,7 +46,7 @@ GPIO.setup(button12, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(button14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 #Set up 3 position switch at IO pins and on/off switch
-GPIO.setup(27,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(21,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(31,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 #Set up these pins to output high to buttons and on/off switch
@@ -194,7 +193,7 @@ while True:
       pizzaData["Weight (lbs)"] = scaleWeight.get()
   
     #Update display corresponding to mode
-    if GPIO.input(27) == GPIO.HIGH:
+    if GPIO.input(21) == GPIO.HIGH:
       updateLightBar(scaleWeight.get(), Pizzas[str(pizzaData["Size"])].cheeseWeight)
     elif GPIO.input(31) == GPIO.HIGH:
       updateLightBar(scaleWeight.get(), Pizzas[str(pizzaData["Size"])].pepWeight)
@@ -217,3 +216,4 @@ with canvas(device) as draw:
         
 #Save last pizza before exiting
 buttonPressed(0)
+GPIO.cleanup()
